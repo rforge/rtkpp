5f5da8ecccd9816ba4a79b647d840079433e3fe6
joblion <joblion@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-12-15 08:40:57 +0000

    Correcting plot method for Poisson models
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rtkpp@59 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/rtkpp/R/ClusterPoisson.R
+++ pkg/rtkpp/R/ClusterPoisson.R
@@ -373,7 +373,7 @@ setMethod(
     signature=c("ClusterPoisson"),
     function(x, y, ...)
     { # use generic plot
-      .clusterPlot(x, y, .dPoisson,...);
+      .clusterPoissonPlot(x, y, .dPoisson,...);
     }
 )
 
@@ -381,3 +381,78 @@ setMethod(
 # x a vector with the point
 .dPoisson <- function(x, j, k, model)
 { dpois(x, lambda = (model@component@lambda)[k, j]) }
+
+.clusterPoissonPlot <- function(model, y, ddensity,...)
+{
+  # total number of variable in the data set
+  nbVariable = ncol(model@component@data);
+  # no y => display all variables
+  if (missing(y)) { y=1:nbVariable; }
+  else # perform some check
+  {
+    if (is.numeric(y)) # numbers of the columns to plot are given
+    {
+      if (max(y)>nbVariable)
+        stop("In .clusterPoissonPlot, y indices mismatch the data dimension")
+    }
+    else # names of the variables to plot are given
+    {
+      if ( sum(y %in% colnames(model@component@data))!= length(y) )
+      { stop(cat("In .clusterPoissonPlot, unknown variable: ", paste(y[which(!(y %in% colnames(model@component@data)))]),"\n"))}
+    }
+  }
+  # get old par
+  op <- par(no.readonly = TRUE) # the whole list of settable par's.
+  # cluster parameters
+  par(mar = rep(2.5,4), cex = .75, oma = c(0, 0, 3, 0))        # margin and font size
+  nbCol = length(y)                       # size of the matrix screen
+  split.screen(c(nbCol, nbCol))           # create layout matrix screens
+  col = model@zi+2;                       # color for each group
+  pch = rep(1, length.out = length(col)); # circles
+  pch[model@component@missing[,1]] = 3;            # + for missing values
+  # create histograms on the diagonal
+  for ( i in 1:nbCol )
+  {
+    screen(i+((i-1)*nbCol))   # sreen(i,i)
+    xValues<-(min(model@component@data[,y[i]]):max(model@component@data[,y[i]]))
+    density<-matrix(nrow=model@nbCluster, ncol=length(xValues))
+    # loop over the clusters to generate densities
+    for( k in 1:model@nbCluster )
+    {  density[k,]<- ddensity(xValues, y[i], k, model);}
+    # generate mixture density
+    mixture<-apply(density,2,sum)
+    if (is.numeric(y)) { xlab=colnames(model@component@data)[y[i]];}
+    else               { xlab= y[i];}
+    # TODO: check if xlab is empty
+    main=paste("Histogram of",xlab)
+    h<-hist(model@component@data[,y[i]], xlab=xlab, main=main, ...)
+    # add on the histogram the estimated densities
+    ratio<-max(h$counts)/max(mixture)
+    density<-density*ratio
+    mixture<-mixture*ratio
+    lines(xValues,mixture,col="azure4", lty=1, lwd=4)
+    for( k in 1:model@nbCluster )
+    { lines(xValues, density[k,], col=k+1, lty=2, lwd=2)}
+  }
+  # add biplots
+  if (nbCol>1)
+  {
+    for ( i in 2:nbCol )
+    {
+      if (is.numeric(y)) { xlab=colnames(model@component@data)[y[i]];}
+      else               { xlab= y[i];}
+      for( j in 1:(i-1) )
+      {
+        screen(j+((i-1)*nbCol)) # screen(i,j)
+        if (is.numeric(y)) {ylab=colnames(model@component@data)[y[j]];}
+        else {ylab= y[j];}
+        plot(model@component@data[,y[j]], model@component@data[,y[i]], col=col, pch=pch, xlab=xlab, ylab=ylab, ...)
+      }
+    }
+  }
+#  mtext("Visualisation using latent logistic representation", outer = TRUE, cex = 1.5)
+  close.screen(all.screens = TRUE)
+  # restore plotting parameters
+  par(op)
+  invisible()
+}
