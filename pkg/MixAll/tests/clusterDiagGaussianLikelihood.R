15d6a0e168317fddb4c432da9f67225868cdadab
joblion <joblion@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-03-13 15:22:14 +0000

    Removing vignettes and modifying tests
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rtkpp@78 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/MixAll/tests/clusterDiagGaussianLikelihood.R
+++ pkg/MixAll/tests/clusterDiagGaussianLikelihood.R
@@ -5,16 +5,17 @@
 library(MixAll)
 data(iris)
 
-gauss_model <- clusterDiagGaussian(iris[1:4], nbCluster = 3:4, modelNames = clusterDiagGaussianNames()
-              , strategy = clusterFastStrategy(), criterion = "ICL")
+gauss_model <- clusterDiagGaussian( iris[1:4], nbCluster = 3, modelNames= c("gaussian_pk_sjk")
+                                  , strategy = clusterFastStrategy())
 
 data<-gauss_model@component@data
-nbSample <- nrow(data)
-nbVariable <- ncol(data)
 nbCluster <- gauss_model@nbCluster
 prop <- gauss_model@pk
 mean <- gauss_model@component@mean
 sigma <- gauss_model@component@sigma
+
+nbSample <- nrow(data)
+nbVariable <- ncol(data)
 
 f <-vector(length=nbSample)
 lnComp <- vector(length=nbCluster)
@@ -30,5 +31,7 @@ for (i in 1:nbSample)
   f[i] = log(sum(exp(lnComp))) + lmax;
 }
 
-cat("Computed log-likelihood: ", sum(f), "\n")
-cat("Model log-likelihood: ", gauss_model@lnLikelihood, "\n")
+if( abs(sum(f) - gauss_model@lnLikelihood) < 1.e-15 )
+{ print ("clusterDiagGaussianLikelihood failed")
+} else
+{ print ("clusterDiagGaussianLikelihood successful") }
--- pkg/MixAll/tests/clusterGammaLikelihood.R
+++ pkg/MixAll/tests/clusterGammaLikelihood.R
@@ -5,16 +5,17 @@
 library(MixAll)
 data(iris)
 
-gamma_model <- clusterGamma( iris[1:4], nbCluster = 3:4, modelNames = clusterGammaNames(shapeBetweenCluster = "all")
+gamma_model <- clusterGamma( iris[1:4], nbCluster = 3, modelNames = c("gamma_pk_ajk_bjk")
                            , strategy = clusterFastStrategy())
 
 data<-gamma_model@component@data
-nbSample <- nrow(data)
-nbVariable <- ncol(data)
 nbCluster <- gamma_model@nbCluster
 prop <- gamma_model@pk
 shape <- gamma_model@component@shape
 scale <- gamma_model@component@scale
+
+nbSample <- nrow(data)
+nbVariable <- ncol(data)
 
 f <-vector(length=nbSample)
 lnComp <- vector(length=nbCluster)
@@ -30,6 +31,6 @@ for (i in 1:nbSample)
 
   f[i] = log(sum(exp(lnComp))) + lmax;
 }
-
-cat("Computed log-likelihood: ", sum(f), "\n")
-cat("Model log-likelihood: ", gamma_model@lnLikelihood, "\n")
+if (abs(sum(f) - gamma_model@lnLikelihood) < 1.e-15)
+{ print ("clusterGammaLikelihood failed")
+} else{ print ("clusterGammaLikelihood successful")  }
