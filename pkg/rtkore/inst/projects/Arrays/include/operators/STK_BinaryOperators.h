0460c2e924afaef36783eb4a24e5a7c78e961758
joblion <joblion@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-12-12 07:01:45 +0000

    bug correction in BinaryOperator vector_+point_
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rtkpp@153 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/rtkore/inst/projects/Arrays/include/operators/STK_BinaryOperators.h
+++ pkg/rtkore/inst/projects/Arrays/include/operators/STK_BinaryOperators.h
@@ -1109,9 +1109,9 @@ struct BinaryEltImpl< FunctorOp, Lhs, Rhs, Arrays::vector_, Arrays::point_>
   typedef typename FunctorOp::result_type result_type;
   enum { structure_     = Arrays::vector_ // vector + point is vector (somehow an arbitrary choice)
        , binary_op_Kind_= Arrays::binary_op_1D_
-       , sizeRows_      = Lhs::sizeRows_ != UnknownSize ? Lhs::sizeRows_ : Rhs::sizeCols_
+       , sizeRows_      = Lhs::sizeRows_ != UnknownSize ? int(Lhs::sizeRows_) : int(Rhs::sizeCols_)
        , sizeCols_      = 1
-       , useLhsRows_    = Lhs::sizRows != UnknownSize ? Arrays::useLhsSize_ : Arrays::useRhsOtherSize_
+       , useLhsRows_    = Lhs::sizRows_ != UnknownSize ? Arrays::useLhsSize_ : Arrays::useRhsOtherSize_
        , useLhsCols_    = Arrays::useLhsSize_
        };
   inline static result_type elt2Impl(FunctorOp const& f, Lhs const& l, Rhs const& r, int i, int j)
