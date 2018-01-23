9ef35c4ba409608ae0c3291b53bbe950487d1e66
joblion <joblion@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-01-23 21:03:08 +0000

    Correcting warning when using StaticAssert
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rtkpp@159 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/rtkore/inst/projects/Sdk/include/STK_StaticAssert.h
+++ pkg/rtkore/inst/projects/Sdk/include/STK_StaticAssert.h
@@ -49,52 +49,52 @@
 namespace STK
 {
 
-template<int condition> struct StaticAssert;
+template<bool condition> struct StaticAssert {};
 // if the condition is false the compiler will complain it does not find the message
-template<> struct StaticAssert<0> {};
-template<> struct StaticAssert<1>
+template<> struct StaticAssert<false> {};
+template<> struct StaticAssert<true>
 {
   enum
   {
-    YOU_TRIED_CALLING_A_VECTOR_METHOD_ON_A_MATRIX,
-    YOU_TRIED_CALLING_A_MATRIX_METHOD_ON_A_VECTOR,
-    YOU_TRIED_CALLING_A_POINT_METHOD_ON_SOMETHING_ELSE,
-    YOU_TRIED_CALLING_A_VECTOR_METHOD_ON_SOMETHING_ELSE,
-    YOU_TRIED_TO_CONSTRUCT_A_REFERENCE_WITH_A_DIFFERENT_ORIENTATION,
-    YOU_TRIED_TO_CONSTRUCT_A_SQUAREMATRIX_WITH_DIFFERENT_DIMENSIONS,
-    YOU_TRIED_TO_CONSTRUCT_A_VECTOR_WITH_MORE_THAN_ONE_COLUM,
-    YOU_TRIED_TO_CONSTRUCT_A_POINT_WITH_MORE_THAN_ONE_ROW,
-    YOU_TRIED_TO_CONSTRUCT_A_SCALAR_WITH_MORE_THAN_ONE_ROW_OR_ONE_COLUMN,
-    YOU_TRIED_TO_APPLY_A_BINARY_OPERATOR_WITH_WRONG_ROWS_SIZE,
-    YOU_TRIED_TO_APPLY_A_BINARY_OPERATOR_WITH_WRONG_COLS_SIZE,
-    YOU_TRIED_TO_APPLY_A_BINARY_OPERATOR_BETWEEN_NOT_COMPATIBLE_ARRAYS,
-    YOU_TRIED_TO_ASSIGN_A_NOT_COMPATIBLE_ARRAY,
-    YOU_TRIED_TO_APPLY_A_METHOD_BETWEEN_NOT_COMPATIBLE_ARRAYS,
-    YOU_TRIED_TO_APPLY_A_PRODUCT_OPERATOR_BETWEEN_NOT_COMPATIBLE_ARRAYS,
-    YOU_TRIED_TO_APPLY_A_BINARY_OPERATOR_WITH_WRONG_DIMENSIONS_SIZE,
-    YOU_TRIED_TO_WRAP_A_CONTAINER_WITH_THE_WRONG_TYPE_AS_A_VARIABLE,
-    YOU_TRIED_TO_WRAP_A_CONTAINER_WHICH_IS_NOT_1D_AS_A_VARIABLE,
-    YOU_TRIED_TO_USE_A_UNIDIMENSIONAL_METHOD_ON_A_MATRIX,
-    YOU_CANNOT_USED_AN_UNIDIMENSIONAL_METHOD_ON_THIS_OBJECT,
-    YOU_CANNOT_USED_A_ZERODIMENSIONAL_METHOD_ON_THIS_OBJECT,
-    YOU_CANNOT_USED_THIS_TYPE_OF_DATA_WITH_THIS_OBJECT,
-    YOU_CANNOT_USED_THIS_METHOD_WITH_THIS_KIND_OF_ARRAY,
-    YOU_HAVE_TO_USE_TWO_ARRAYS_WITH_THE_SAME_ORIENTATION,
-    YOU_HAVE_TO_USE_A_VECTOR_OR_POINT_IN_THIS_METHOD,
-    YOU_HAVE_TO_USE_A_SQUARE_MATRIX_IN_THIS_METHOD,
-    YOU_HAVE_TO_USE_A_MATRIX_OR_SQUARE_MATRIX_IN_THIS_METHOD,
-    INVALID_VECTOR_VECTOR_PRODUCT,
-    INVALID_POINT_POINT_PRODUCT
+    YOU_TRIED_CALLING_A_VECTOR_METHOD_ON_A_MATRIX=0,
+    YOU_TRIED_CALLING_A_MATRIX_METHOD_ON_A_VECTOR=0,
+    YOU_TRIED_CALLING_A_POINT_METHOD_ON_SOMETHING_ELSE=0,
+    YOU_TRIED_CALLING_A_VECTOR_METHOD_ON_SOMETHING_ELSE=0,
+    YOU_TRIED_TO_CONSTRUCT_A_REFERENCE_WITH_A_DIFFERENT_ORIENTATION=0,
+    YOU_TRIED_TO_CONSTRUCT_A_SQUAREMATRIX_WITH_DIFFERENT_DIMENSIONS=0,
+    YOU_TRIED_TO_CONSTRUCT_A_VECTOR_WITH_MORE_THAN_ONE_COLUM=0,
+    YOU_TRIED_TO_CONSTRUCT_A_POINT_WITH_MORE_THAN_ONE_ROW=0,
+    YOU_TRIED_TO_CONSTRUCT_A_SCALAR_WITH_MORE_THAN_ONE_ROW_OR_ONE_COLUMN=0,
+    YOU_TRIED_TO_APPLY_A_BINARY_OPERATOR_WITH_WRONG_ROWS_SIZE=0,
+    YOU_TRIED_TO_APPLY_A_BINARY_OPERATOR_WITH_WRONG_COLS_SIZE=0,
+    YOU_TRIED_TO_APPLY_A_BINARY_OPERATOR_BETWEEN_NOT_COMPATIBLE_ARRAYS=0,
+    YOU_TRIED_TO_ASSIGN_A_NOT_COMPATIBLE_ARRAY=0,
+    YOU_TRIED_TO_APPLY_A_METHOD_BETWEEN_NOT_COMPATIBLE_ARRAYS=0,
+    YOU_TRIED_TO_APPLY_A_PRODUCT_OPERATOR_BETWEEN_NOT_COMPATIBLE_ARRAYS=0,
+    YOU_TRIED_TO_APPLY_A_BINARY_OPERATOR_WITH_WRONG_DIMENSIONS_SIZE=0,
+    YOU_TRIED_TO_WRAP_A_CONTAINER_WITH_THE_WRONG_TYPE_AS_A_VARIABLE=0,
+    YOU_TRIED_TO_WRAP_A_CONTAINER_WHICH_IS_NOT_1D_AS_A_VARIABLE=0,
+    YOU_TRIED_TO_USE_A_UNIDIMENSIONAL_METHOD_ON_A_MATRIX=0,
+    YOU_CANNOT_USED_AN_UNIDIMENSIONAL_METHOD_ON_THIS_OBJECT=0,
+    YOU_CANNOT_USED_A_ZERODIMENSIONAL_METHOD_ON_THIS_OBJECT=0,
+    YOU_CANNOT_USED_THIS_TYPE_OF_DATA_WITH_THIS_OBJECT=0,
+    YOU_CANNOT_USED_THIS_METHOD_WITH_THIS_KIND_OF_ARRAY=0,
+    YOU_HAVE_TO_USE_TWO_ARRAYS_WITH_THE_SAME_ORIENTATION=0,
+    YOU_HAVE_TO_USE_A_VECTOR_OR_POINT_IN_THIS_METHOD=0,
+    YOU_HAVE_TO_USE_A_SQUARE_MATRIX_IN_THIS_METHOD=0,
+    YOU_HAVE_TO_USE_A_MATRIX_OR_SQUARE_MATRIX_IN_THIS_METHOD=0,
+    INVALID_VECTOR_VECTOR_PRODUCT=0,
+    INVALID_POINT_POINT_PRODUCT=0
   };
 };
 
 } // namespace STK
 
-#ifndef IS_RTKPP_LIB
-#pragma GCC diagnostic ignored "-Wint-in-bool-context"
-#endif
+//#ifndef IS_RTKPP_LIB
+//#pragma GCC diagnostic ignored "-Wint-in-bool-context"
+//#endif
 #define STK_STATIC_ASSERT(COND,MSG) \
-if (STK::StaticAssert<bool(COND)>::MSG) {}
+if (STK::StaticAssert<COND>::MSG) {}
 
 #else /* C++11  */
 
@@ -138,8 +138,8 @@ if (STK::StaticAssert<bool(COND)>::MSG) {}
     STK_STATIC_ASSERT(COND,YOU_TRIED_TO_APPLY_A_PRODUCT_OPERATOR_BETWEEN_NOT_COMPATIBLE_ARRAYS)
 
 #define STK_STATIC_ASSERT_TWO_DIMENSIONS_ONLY(EXPR) \
-STK_STATIC_ASSERT( (int(hidden::Traits<EXPR>::structure_)==(int)Arrays::array2D_) \
-                 ||(int(hidden::Traits<EXPR>::structure_)==(int)Arrays::square_) \
+STK_STATIC_ASSERT( ((int(hidden::Traits<EXPR>::structure_)==(int)Arrays::array2D_)) \
+                 ||(int(hidden::Traits<EXPR>::structure_)==(int)Arrays::square_)       \
                  ||(int(hidden::Traits<EXPR>::structure_)==(int)Arrays::diagonal_) \
                  ||(int(hidden::Traits<EXPR>::structure_)==(int)Arrays::number_) \
                  ||(int(hidden::Traits<EXPR>::structure_)==(int)Arrays::lower_triangular_) \
@@ -150,22 +150,22 @@ STK_STATIC_ASSERT( (int(hidden::Traits<EXPR>::structure_)==(int)Arrays::array2D_
                  ,YOU_TRIED_CALLING_A_MATRIX_METHOD_ON_A_VECTOR)
 
 #define STK_STATIC_ASSERT_ONE_DIMENSION_ONLY(EXPR) \
-STK_STATIC_ASSERT(  STK_SINGLE_ARG2(hidden::IsEqual< int(hidden::Traits<EXPR>::structure_), int(Arrays::vector_)>::value_) \
-                  ||STK_SINGLE_ARG2(hidden::IsEqual< int(hidden::Traits<EXPR>::structure_), int(Arrays::point_)>::value_) \
-                  ||STK_SINGLE_ARG2(hidden::IsEqual< int(hidden::Traits<EXPR>::structure_), int(Arrays::diagonal_)>::value_) \
-                  ||STK_SINGLE_ARG2(hidden::IsEqual< int(hidden::Traits<EXPR>::structure_), int(Arrays::number_)>::value_) \
-                  ,YOU_CANNOT_USED_AN_UNIDIMENSIONAL_METHOD_ON_THIS_OBJECT)
+STK_STATIC_ASSERT( ( int(hidden::Traits<EXPR>::structure_) == int(Arrays::vector_))   \
+                 ||( int(hidden::Traits<EXPR>::structure_) == int(Arrays::point_))    \
+                 ||( int(hidden::Traits<EXPR>::structure_) == int(Arrays::diagonal_)) \
+                 ||( int(hidden::Traits<EXPR>::structure_) == int(Arrays::number_))   \
+                 ,YOU_CANNOT_USED_AN_UNIDIMENSIONAL_METHOD_ON_THIS_OBJECT)
 
 #define STK_STATIC_ASSERT_SAME_ORIENT(LHS, RHS) \
-STK_STATIC_ASSERT( hidden::Traits<LHS>::structure_== hidden::Traits<RHS>::structure_ \
+STK_STATIC_ASSERT( int(hidden::Traits<LHS>::structure_)== int(hidden::Traits<RHS>::structure_) \
                  ,YOU_HAVE_TO_USE_TWO_ARRAYS_WITH_THE_SAME_ORIENTATION)
 
 #define STK_STATIC_ASSERT_POINT_ONLY(EXPR) \
-STK_STATIC_ASSERT(int(hidden::Traits<EXPR>::structure_) == int(Arrays::point_) \
+STK_STATIC_ASSERT( int(hidden::Traits<EXPR>::structure_) == int(Arrays::point_) \
                  ,YOU_TRIED_CALLING_A_POINT_METHOD_ON_SOMETHING_ELSE)
 
 #define STK_STATIC_ASSERT_VECTOR_ONLY(EXPR) \
-STK_STATIC_ASSERT(STK_SINGLE_ARG2(hidden::IsEqual< int(hidden::Traits<EXPR>::structure_), int(Arrays::vector_)>::value_) \
+STK_STATIC_ASSERT( int(hidden::Traits<EXPR>::structure_) == int(Arrays::vector_) \
                  ,YOU_TRIED_CALLING_A_VECTOR_METHOD_ON_SOMETHING_ELSE)
 
 #define STK_STATIC_ASSERT_POINT_OR_VECTOR_ONLY(EXPR) \
